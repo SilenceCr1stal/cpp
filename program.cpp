fd405e8ccbb5d1c8f549ae192ca9ecb197ef4bf2
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-11-01 01:41:34 +0300

    dynamic memory

--- program.cpp
+++ program.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    int age = 17;
-    int *pointer = &age;
-    cout << pointer << endl;
-    int someNum = 34;
-    *pointer = someNum + *pointer;
-    cout << *pointer << endl;
+    int *age = new int;
+    *age = 17;
+    cout << *age << endl;
+    delete age;
+    age = new int;
+    *age = 18;
+    cout << *age << endl;
+    delete age;
     return 0;
 }
