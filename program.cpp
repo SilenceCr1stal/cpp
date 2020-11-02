b4829bd81030285f0b5c810e4796fa59b0d62a20
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-11-02 04:10:43 +0300

    bit

--- program.cpp
+++ program.cpp
@@ -2,23 +2,16 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-    cout << sizeof(int) << endl;
-    int *p = NULL;
-    cout << sizeof(*p) << " " << sizeof(p) << " " << p << endl;
-    int age = 17;
-    cout << "sizeof \'age\' = " << sizeof(age) << endl;
-    string name = "sizeof empty strsizeof empty strsizeof empty strsizeof empty strsizeof empty strsizeof empty strsizeof empty strsizeof empty strsizeof empty str";
-    cout << "sizeof empty str = " << sizeof(string) << " and name = " << sizeof(name) << endl;
-
-    int arr[33];
-    cout << "Quantity of elements in a array: " << sizeof(arr) / sizeof(arr[0]) << endl;
+    unsigned short int age = 17;
+    int num = age << 2; // 68
+    cout << num << endl;
+    int num2 = age >> 3; // 2
+    cout << num2 << endl;
+    age |= num; // 85
+    cout << age << endl;
+    age ^= num2; // 87
+    cout << age << endl;
+    num &= num2; // 0
+    cout << num << endl;
     return 0;
 }
-
-/*
-4
-4 8 0
-sizeof 'age' = 4
-sizeof empty str = 32 and name = 32
-Quantity of elements in a array: 33
-*/
