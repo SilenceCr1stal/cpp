b1a436a7a39ea33613c6da883b315d455f27e0ff
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-11-06 03:09:40 +0300

    composition

--- program.cpp
+++ program.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
-#include <string.h>
-#include "human.h"
 using namespace std;
 
-human::human(int a, string _name) {
-    cout << "Created" << endl;
-    human::setAge(a);
-    name = _name;
-}
 
-string human::getName() const {
-    return name;
-}
-
-void human::setAge(int a) {
-    human::age = a;
-}
-
-int human::getAge() const {
-    return human::age;
-}
-
-human::~human() {
-    cout << "Destroyed" << endl;
-}
 
 int main(int argc, char **argv) {
-    const human human1(17, "Artem"); // Created
-    cout << human1.getName() << endl; // Artem
-    const human *p = &human1;
-    cout << p->getAge() << endl; // 17
-    return 0; // Destroyed
+    cout << "Hello" << endl;
+    return 0;
 }
