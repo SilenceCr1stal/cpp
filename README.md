3435d6ab719dd7c4939ef7ce5ec52cc26ddf5638
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-12-16 01:59:14 +0300

    template functions

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### `It's simple cpp repository, nothing special`
 
-### `There are 7 branches, each with it's own specifics`
+### `There are 8 branches, each with it's own specifics`
 
 ### `The repository will be regularly updated with new code`
--- program.cpp
+++ program.cpp
@@ -1,59 +1,17 @@
 #include <iostream>
-#include <string.h>
-#include <time.h>
-#include "human.h"
-#include "birthday.h"
+#include <ctime>
+
 using namespace std;
 
-human::human(int a, string _name, birthday _date) : age(a), name(_name), date(_date) {
-    cout << "Created" << endl;
-}
-
-string human::getName() const {
-    return name;
-}
-
-void human::printInfo() {
-    cout << name << endl;
-    date.printDate();
-}
-
-int human::getAge() const {
-    return age;
-}
-
-human::~human() {
-    cout << "Destroyed" << endl;
-    time_t rawtime;
-    struct tm *timeinfo;
-
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    cout << "date of death:" << asctime(timeinfo) << endl;
-}
-
-
-birthday::birthday(int _day, int _month, int _year) : day(_day), month(_month), year(_year) {}
-
-void birthday::printDate() {
-    cout << day << "/" << month << "/" << year << endl;
-}
-
-birthday::~birthday() {}
-
+template <class A, class B>
+B getSomething(A a, B b);
 
 int main(int argc, char **argv) {
-    const birthday *date = new birthday(31, 12, 2000);
-    human *human1 = new human(19, "Artem", *date);
-    human1->printInfo();
-    delete date;
-    delete human1;
+    cout << getSomething(17, 8.6) << endl;
+    return 0;
 }
 
-/*
-Created
-Artem
-31/12/2000
-Destroyed
-date of death:Fri Nov  6 03:07:42 2020
-*/
+template <class A, class B>
+B getSomething(A a, B b) {
+    return a + b;
+}
