8d9df45eb375cbc7297054e83d5ba4c6ab3238df
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-12-17 19:32:36 +0300

    pointers and links

--- program.cpp
+++ program.cpp
@@ -3,14 +3,70 @@
 #include <sstream>
 using namespace std;
 
+void change(int *a, int *b);
 int main(int argc, char **argv) {
-    int *age = new int;
-    *age = 17;
-    cout << *age << endl;
-    delete age;
-    age = new int;
-    *age = 18;
-    cout << *age << endl;
-    delete age;
+    
+    int myMark = 5;
+
+    int otherMark = 3;
+
+    const unsigned short int size = 6;
+
+    int arr[size] = {17, 16, 15, 14, 13, 12};
+
+    for (int i = 0; i < size; i++) {
+        cout << *(arr + i) << endl;
+        switch (i) {
+            case size - 1:
+                cout << "======================" << endl;
+                break;
+        }
+    }
+
+    int *pArr = arr;
+
+    *(pArr + 3) = 11;
+
+    // cout << arr[3] << endl; // 11
+
+    int mark = 4;
+
+    int *pmark = &mark;
+
+    // cout << pmark << " " << *pmark << endl; // 0x7ffdafb01ddc 4
+
+    *pmark = 5;
+
+    // cout << mark << endl; // 5
+
+    int **ppmark = &pmark;
+
+    // cout << ppmark << " " << **ppmark << endl; // 0x7ffdafb01dd0 5
+
+    **ppmark = 2;
+
+    // cout << mark << endl << "----------------------" << endl; // 2
+    
+    // cout << myMark << " " << otherMark << endl;
+
+    change(&myMark, &otherMark);
+
+    cout << myMark << endl;
+
+    cout << otherMark << endl;
+
+
+    int &a = otherMark;
+
+    a++;
+
+    cout << otherMark << endl;
+
     return 0;
+}
+
+void change(int *a, int *b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
 }
