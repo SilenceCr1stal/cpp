fa4cee3640b47416e555c83c093e65cfc297da8e
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-12-20 01:17:48 +0300

    three-dimensional array

--- program.cpp
+++ program.cpp
@@ -2,28 +2,30 @@
 #include <string>
 #include <time.h>
 #define SIZE 5
-#define SIZE_COL 4
+#define SIZE_LOW 4
+#define SIZE_LOW_LOW 3
 
 int main(int argc, char** argv) {
     srand(time(NULL));
-    // int nums[] = {17, 87, 54, 49, 16, 31, 70};
-
-
-    int **ages = new int* [SIZE];
+    
+    int ***nums = new int** [SIZE];
 
     for (int i = 0; i < SIZE; i++) {
-        *(ages + i) = new int[SIZE_COL];
-        for (int j = 0; j < SIZE_COL; j++) {
-            *(*(ages + i) + j) = rand() % 10;
-            std::cout << *(*(ages + i) + j) << "\t";
+        nums[i] = new int* [SIZE_LOW];
+        for (int j = 0; j < SIZE_LOW; j++) {
+            *(*(nums + i) + j) = new int [SIZE_LOW_LOW];
+            for (int k = 0; k < SIZE_LOW_LOW; k++) {
+                *(*(*(nums + i) + j) + k) = rand() % 100;
+                std::cout << *(*(*(nums + i) + j) + k) << "\t";
+            }
+            std::cout << "===================\t";
+            delete [] *(*(nums + i) + j);
         }
         std::cout << std::endl;
-        if (i == SIZE_COL) {
-                delete [] *(ages + i);
-            }
+        delete [] *(nums + i);
     }
 
-    delete [] ages;
+    delete [] nums;
 
     return 0;
 }
