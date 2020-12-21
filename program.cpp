15e8304666b7e841deba9c2b08d9399a83973f24
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-12-22 01:54:42 +0300

    functions dynamic arrays

--- program.cpp
+++ program.cpp
@@ -5,27 +5,68 @@
 #define SIZE_LOW 4
 #define SIZE_LOW_LOW 3
 
+void FillArray(int** nums, const int size) {
+    for (int i = 0; i < size; i++) {
+        *(nums + i) = new int [size];
+        for (int j = 0; j < size; j++) {
+            *(*(nums + i) + j) = rand() % 100;
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     srand(time(NULL));
     
-    int ***nums = new int** [SIZE];
+    int** nums = new int* [SIZE];
+
+    FillArray(nums, SIZE);
 
     for (int i = 0; i < SIZE; i++) {
-        nums[i] = new int* [SIZE_LOW];
-        for (int j = 0; j < SIZE_LOW; j++) {
-            *(*(nums + i) + j) = new int [SIZE_LOW_LOW];
-            for (int k = 0; k < SIZE_LOW_LOW; k++) {
-                *(*(*(nums + i) + j) + k) = rand() % 100;
-                std::cout << *(*(*(nums + i) + j) + k) << "\t";
-            }
-            std::cout << "===================\t";
-            delete [] *(*(nums + i) + j);
+        for (int j = 0; j < SIZE; j++) {
+            std::cout << *(*(nums + i) + j) << std::endl;
         }
-        std::cout << std::endl;
-        delete [] *(nums + i);
+        if (i == SIZE - 1) {
+            std::cout << "======================" << std::endl;
+            }
     }
+
+    int** nums2 = new int* [SIZE_LOW_LOW];
+
+    FillArray(nums2, SIZE_LOW_LOW);
+
+    for (int i = 0; i < SIZE_LOW_LOW; i++) {
+        for (int j = 0; j < SIZE_LOW_LOW; j++) {
+            std::cout << *(*(nums2 + i) + j) << std::endl;
+        }
+        if (i == SIZE_LOW_LOW - 1) {
+            std::cout << "======================" << std::endl;
+            }
+    }
+
+    delete [] nums;
+
+    nums = new int* [SIZE_LOW_LOW];
+
+    for (int i = 0; i < SIZE_LOW_LOW; i++) {
+        *(nums + i) = new int [SIZE_LOW_LOW];
+        for (int j = 0; j < SIZE_LOW_LOW; j++) {
+            *(*(nums + i) + j) = *(*(nums2 + i) + j);
+        }
+    }
+
+    for (int i = 0; i < SIZE_LOW_LOW; i++) {
+        for (int j = 0; j < SIZE_LOW_LOW; j++) {
+            std::cout << *(*(nums + i) + j) << std::endl;
+        }
+        if (i == SIZE_LOW_LOW - 1) {
+            std::cout << "======================" << std::endl;
+            }
+    }
+
+    delete [] nums2;
 
     delete [] nums;
 
     return 0;
 }
+
