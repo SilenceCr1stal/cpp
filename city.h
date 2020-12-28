25a1d4ef03f55e3a0f60c38186f43066e9c92233
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-12-28 18:27:10 +0300

    classes

--- city.h
+++ city.h
@@ -14,21 +14,5 @@ class city {
         friend void aboutMen(human &human1);
 };
 
-<<<<<<< HEAD
-=======
-city::city(unsigned int _square, std::string _name, building *_build1) : square(_square), name(_name), build1(*_build1) {
-    std::cout << "Created" << std::endl;
-}
-
-void city::printInfo() {
-    std::cout << "Square: " << this->square << std::endl;
-    std::cout << "Name country: " << (*this).name << std::endl;
-    build1.printInfo();
-}
-
-city::~city() {
-    std::cout << "Destroyed" << std::endl;
-}
->>>>>>> edae2ff31295e7674aead6752619370dc778c3e2
 
 #endif // !CITY_H
--- human.h
+++ human.h
@@ -17,34 +17,4 @@ class human {
         friend void checkAge(human &human1);
 };
 
-<<<<<<< HEAD
-=======
-human::human(std::string _name, const birthday *_date) : name(_name), date(*_date) {}
-
-void human::printInfo() {
-    std::cout << name << std::endl;
-    date.printDate();
-}
-
-int human::getAge() const {
-    return age;
-}
-
-std::string human::getName() const {
-    return name;
-}
-
-void checkAge(human &human1) {
-    human1.age = 18;
-}
-
-void human::dateDeath() {
-    time_t rawtime;
-    struct tm *timeinfo;
-
-    timeinfo = localtime(&rawtime);
-    std::cout << "date of death: " << asctime(timeinfo) << std::endl;
-}
->>>>>>> edae2ff31295e7674aead6752619370dc778c3e2
-
 #endif // !HUMAN_H
