920cc7908c5585a76ba2919bfeb84cb6b419cb82
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-12-29 05:09:02 +0300

    palindrome function

--- program.cpp
+++ program.cpp
@@ -1,73 +1,77 @@
 #include <iostream>
 #include <string.h>
 #include <ctime>
-#include "city.h"
-#include "building.h"
-#include "human.h"
-#include "birthday.h"
 using namespace std;
 
-city::city(unsigned int _square, string _name, building *_build1) : square(_square), name(_name), build1(*_build1) {
-    cout << "Created" << endl;
-}
+bool isPalindrome(int num, int& pI);
 
-void city::printInfo() {
-    cout << "Square: " << square << endl;
-    build1.printInfo();
-}
-
-city::~city() {
-    cout << "Destroyed" << endl;
-}
-
-building::building(int _height, human *_human1) : human1(*_human1) {
-    building::setHeight(_height);
-}
-
-void building::setHeight(int _height) {
-    height = _height;
-}
-
-void building::printInfo() {
-    cout << "Height of building: " << height << endl;
-    human1.printInfo();
-}
-
-human::human(string _name, const birthday *_date) : name(_name), date(*_date) {}
-
-void human::printInfo() {
-    cout << name << endl;
-    date.printDate();
-}
-
-void human::dateDeath() {
-    time_t rawtime;
-    struct tm *timeinfo;
-
-    timeinfo = localtime(&rawtime);
-    cout << "date of death: " << asctime(timeinfo) << endl;
-}
-
-birthday::birthday(int _day, int _month, int _year) : day(_day), month(_month), year(_year) {}
-
-void birthday::printDate() {
-    cout << day << "/" << month << "/" << year << endl;
+int level(int num) {
+    if (num == 1) {
+        return 10;
+    } else {
+        return 10 * level(num - 1);
+    }
 }
 
 int main(int argc, char **argv) {
-    const birthday *date = new birthday(29, 12, 2002);
-
-    human *human1 = new human("Nikita", date);
-
-    building *build1 = new building(1000, human1);
-
-    city *town = new city(3800000, "USA", build1);
-
-    town->printInfo();
-    delete town;
-    delete build1;
-    delete human1;
-    delete date;
     
+    int n;
+
+    int i = 0;
+
+    int* pI = &i;
+
+    std::cin >> n;
+
+    int symbols = n;
+
+    for (;;) {
+        if (symbols > 10) {
+            symbols /= 10;
+            ++i;
+        } else {
+            *pI = *pI + 1;
+            break;
+        }
+    }
+
+    if (isPalindrome(n, *pI)) {
+        std::cout << n << " is a palindrome" << std::endl;
+    } else {
+        std::cout << n << " is NOT a palindrome" << std::endl;
+    }
+
     return 0;
 }
+
+bool isPalindrome(int num, int& pI) {
+    pI /= 2;
+    int n = num;
+    int n2 = num;
+    for (int i = pI; i >= 1; i--) {
+        for (;;) {
+            if (n > level(i)) {
+                n /= 10;
+                if (n < level(i)) {
+                    n %= 10;
+                    n2 %= level(i);
+                    if (i != 1) {
+                        n2 /= level(i-1);
+                    }
+                    if (n != n2) {
+                        return false;
+                    }
+                }
+            } else {
+                n = num;
+                n2 = num;
+                break;
+            }
+        }
+    }
+    return true;
+}
+
+// 12321
+
+// 1234321
