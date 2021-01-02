37b043ed4b2a06f73ccec0ccdc9abd5496cdfae0
SilenceCr1stal <nikitapolyashov2@gmail.com>
2021-01-03 01:51:29 +0300

    array

--- program.cpp
+++ program.cpp
@@ -2,40 +2,53 @@
 #include <cmath>
 #include <string>
 #include <string.h>
+#define SIZE 5
 
-template<typename Type>
-Type getSum(Type a, Type b) {
-    return a + b;
-}
-template<typename Type>
-Type getComp(Type a, Type b) {
-    return a * b;
-}
-template<typename Type>
-Type getDiv(Type a, Type b) {
-    return a / b;
+void FillArray(int*** array, const int size) {
+    for (short int i = 0; i < SIZE; i++) {
+        array[i] = new int* [size - 1];
+        for (short int j = 0; j < size - 1; j++) {
+            *(*(array + i) + j) = new int [size - 1 - 1];
+            for (short int k = 0; k < size - 1 - 1; k++) {
+                *(*(*(array + i) + j) + k) = rand() % 100;
+            }
+        }
+    }
 }
 
-template<typename Type>
-void ShowInfo(Type(*func)(Type a, Type b), Type a, Type b) {
-    std::cout << func(a, b) << std::endl;
+void ShowArray(int*** array, const int size) {
+    for (short int i = 0; i < size; i++) {
+        for (short int j = 0; j < size - 1; j++) {
+            for (short int k = 0; k < size - 1 - 1; k++) {
+                if (!(k == size - 1 - 1 - 1)) {
+                    std::cout << array[i][j][k] << "\t";
+                } else if (j == size - 1 - 1) {
+                    std::cout << array[i][j][k];
+                } else {
+                    std::cout << array[i][j][k] << "\t===\t";
+                    delete [] array[i][j];
+                }
+            }
+            if (j == size - 1 - 1) {
+                std::cout << std::endl;
+                delete [] array[i];
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv) {
 
     setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
 
-    char* name;
+    int*** array = new int** [SIZE];
 
-    double num1 = 7.5, num2 = 1.5;
+    FillArray(array, SIZE);
 
-    goto sum;
+    ShowArray(array, SIZE);
 
-    std::cin >> name;
-
-    sum:
-
-    ShowInfo(getSum, num1, num2);
+    delete [] array;
 
     return 0;
 }
