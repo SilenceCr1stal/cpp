4a3c0159319825f951801ea45725b1beb3e7c68a
SilenceCr1stal <nikitapolyashov2@gmail.com>
2021-01-07 03:15:05 +0300

    overloading the assignment operator

--- program.cpp
+++ program.cpp
@@ -67,6 +67,18 @@ public:
     //         delete[] types[i];
     //     }
     // }
+    void operator = (const Cable& cable) {
+        if (this->types != nullptr) {
+            delete[] types;
+        }
+        this->types = new char*[COUNT];
+        for (int i = 0; i < COUNT; i++) {
+            types[i] = cable.types[i];
+        }
+        this->color = cable.color;
+        this->length = cable.length;
+        this->price = cable.price;
+    }
 };
 
 int main(int argc, char **argv) {
@@ -80,11 +92,51 @@ int main(int argc, char **argv) {
     cable1.SetColor("white");
     cable1.printInfo(2);
     std::cout << std::endl;
-    Cable cable2(cable1);
+    Cable cable2 = cable1;
     std::cout << std::endl;
     cable2.SetColor("black");
     cable2.printInfo(1);
+
     std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    cable1 = cable2;
+    std::cout << std::endl;
+    cable1.printInfo(2);
+    std::cout << std::endl;
+    std::cout << std::endl;
+    cable2.printInfo(2);
 
     return 0;
 }
+
+/*
+
+Constructor has been called - 0x7ffc57cb7eb0
+
+Function 'printInfo' has been called for object - 0x7ffc57cb7eb0
+Type of cable - Type-A  Color - white
+Price per piece - 150.75        His length - 20
+
+The copy constructor has been called - 0x7ffc57cb7e70
+
+Function 'printInfo' has been called for object - 0x7ffc57cb7e70
+Type of cable - Type-C  Color - black
+Price per piece - 50.25 His length - 40
+
+
+
+
+Function 'printInfo' has been called for object - 0x7ffc57cb7eb0
+Type of cable - Mini-B  Color - black
+Price per piece - 50.25 His length - 40
+
+
+Function 'printInfo' has been called for object - 0x7ffc57cb7e70
+Type of cable - Mini-B  Color - black
+Price per piece - 50.25 His length - 40
+Destructor has been called - 0x7ffc57cb7e70
+Destructor has been called - 0x7ffc57cb7eb0
+
+*/
